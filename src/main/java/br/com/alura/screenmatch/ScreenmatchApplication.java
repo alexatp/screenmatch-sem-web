ca779938902ed48d17e7447630c6e9b1f819d450
Alexandre Teixeira <alextp@gmail.com>
2024-12-14 23:07:38 -0300

    Inicio do projeto spring

---
+++ src/main/java/br/com/alura/screenmatch/ScreenmatchApplication.java
@@ -0,0 +1,31 @@
+package br.com.alura.screenmatch;
+
+import org.springframework.boot.CommandLineRunner;
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+import br.com.alura.screenmatch.model.DadosSerie;
+import br.com.alura.screenmatch.service.ConsumoApi;
+import br.com.alura.screenmatch.service.ConverteDados;
+
+@SpringBootApplication
+public class ScreenmatchApplication implements CommandLineRunner {
+
+	@Override
+	public void run(String... args) throws Exception {
+ 
+		var consumoApi = new ConsumoApi();
+		//var json = consumoApi.obterDados("https://www.omdbapi.com/?t=gilmore+girls&apikey=629eb3bd");
+		var json = consumoApi.obterDados("https://www.omdbapi.com/?t=supernatural&apikey=629eb3bd");
+		System.out.println(json);
+		
+		ConverteDados conversor = new ConverteDados();
+		DadosSerie dados = conversor.obterDados(json,  DadosSerie.class);
+		System.out.println(dados);
+	}
+
+	public static void main(String[] args) {
+		SpringApplication.run(ScreenmatchApplication.class, args);
+	}
+
+}
